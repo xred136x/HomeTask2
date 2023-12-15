d40b932b5af749a4cf6163618ca1c018a6c06254
andrew <andrew@DESKTOP-G5QDJAO>
2023-12-15 14:02:52 +0500

    Добавьте файлы проекта.

---
+++ HomeTask2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace HomeTask2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Последовательность в диапазоне";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(251, 19);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(154, 236);
+            this.textBox3.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 88);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Фибоначчи";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(433, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Приостановить поток";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(433, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Возобновить поток";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(433, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Остановить поток";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(433, 219);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 37);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Остановить поток Фибоначчи";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(433, 78);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(127, 47);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Приостановить Фибоначчи";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(433, 131);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(127, 53);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Возобновить Фибоначчи";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ HomeTask2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HomeTask2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+        int num1, num2;
+        public void ThreadMetod(object sender)
+        {
+            if(textBox1.Text == "") num1 = 2;
+            else
+                num1 = int.Parse(textBox1.Text);
+            num2 = int.Parse(textBox2.Text);
+            for (int i = num1; i < num2; i++)
+            {
+                (sender as TextBox).Text += "поток" + "  "
+                    + Convert.ToString(i) + "\r\n";
+                Thread.Sleep(500);
+            }
+        }
+        
+        public void ThreadMetod2(object sender)
+        {
+            num1 = int.Parse(textBox1.Text);
+            num2 = int.Parse(textBox2.Text);
+            int prev = 0;
+            int current = 1;
+            int temp;
+            for (int i = 0; i <= num2; i++)
+            {                
+                temp = current;
+                current = current + prev;
+                prev = temp;
+                if (current > num1 && current < num2)
+                {
+                    (sender as TextBox).Text += "поток" + "  "
+                    + Convert.ToString(current) + "\r\n";
+                    Thread.Sleep(500);
+                }
+                
+            }
+        }
+        Thread thread;
+        Thread thread1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(ThreadMetod);
+            thread.Start((object)textBox3);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thread1 = new Thread(ThreadMetod2);
+            thread1.Start((object)textBox3);
+            
+        }
+        [Obsolete]
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if(thread.ThreadState == ThreadState.Running || thread.ThreadState == ThreadState.Background)
+                thread.Suspend();
+            
+        }
+        [Obsolete]
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!(thread.ThreadState == ThreadState.Running || thread.ThreadState == ThreadState.Background))
+                thread.Resume();
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+        }
+        [Obsolete]
+        private void button7_Click(object sender, EventArgs e)
+        {
+            thread1.Suspend();
+        }
+        [Obsolete]
+        private void button8_Click(object sender, EventArgs e)
+        {
+            thread1.Resume();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            thread1.Abort();
+        }
+    }
+}
